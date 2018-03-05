ed7d2f57ad4eed6d5169745cd3ea4f707f69aa25
Alex Shevchenko <alexshevchenko9999@gmail.com>
2018-03-04 23:38:10 -0500

    Worked on parser

--- parser.py
+++ parser.py
@@ -32,4 +32,63 @@ The file follows the following format:
 See the file script for an example of the file format
 """
 def parse_file( fname, points, transform, screen, color ):
-    pass
+    file = open(fname, "r")
+    lines = file.readlines()
+    
+    i = 0
+    while (i < len(lines)):
+        
+        if (lines[i] == "line\n"):
+            args = lines[i+1].split(" ")
+            add_edge(points, int(args[0]), int(args[1]), int(args[2]), int(args[3]), int(args[4]), int(args[5]))
+            i+=2
+        
+        elif(lines[i] == "indent\n"):
+            ident(transform)
+            i++
+        
+        elif(lines[i] == "scale\n"):
+            args = lines[i+1].split(" ")
+            mt = make_scale(int(args[0]), int(args[1]), int(args[2]))
+            matrix_mult(mt, transform)
+            i+=2
+
+        elif(lines[i] == "move\n"):
+            args = lines[i+1].split(" ")
+            mt = make_translate(int(args[0]), int(args[1]),int(args[2]))
+            matrix_mult(mt, transform)
+            i+=2
+            
+        elif(lines[i] == "rotate\n"):
+            args = lines[i+1].split(" ")
+            if (rotations[0] == "x"):
+                mt = make_rotX(int(args[1]))
+            if (rotations[0] == "y"):
+                mt = make_rotY(int(args[1]))
+            if (rotations[0] == "z"):
+                mt = make_rotZ(int(args[1]))
+            matrix_mult(mt, transform)
+            i+=2
+
+        elif(lines[i] == "apply\n"):
+            matrix_mult(transform,points)
+            i+=1
+
+        elif(lines[i] == "display\n"):
+            for r in range(len(points)):
+                for c in range(len(points[r])):
+                    points[r][c] = int(points[r][c]) 
+            draw_lines(points, screen, color)
+            display(screen)
+            i+=1
+            
+        elif(lines[i] == "display\n"):
+            save_extension(screen,file_lines[i+1].strip())
+
+        elif(lines[i] == "display\n"):
+            break
+        
+        else:
+            raise Exception("Unknown command")
+
+
